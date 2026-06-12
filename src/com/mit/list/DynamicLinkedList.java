384c68cff96c438e399f678e8c1cbbba177378a9
Sanjyot Dhamal <sanjyotdhamal31@gmail.com>
2026-06-12 15:49:33 +0530

    Refactor DynamicLinkedList and fix method names

--- src/com/mit/list/DynamicLinkedList.java
+++ src/com/mit/list/DynamicLinkedList.java
@@ -1,54 +1,60 @@
 package com.mit.list;
 
 public class DynamicLinkedList {
-	public static void main(String[] args) {
-		SinglyLinkedListDemo linkedList=new SinglyLinkedListDemo();
-		linkedList.insertAtBegining(10);
-		linkedList.insertAtBegining(80);
-		linkedList.traverseSinglyList();
 
-	}
+    public static void main(String[] args) {
 
+        SinglyLinkedListDemo linkedList = new SinglyLinkedListDemo();
+
+        linkedList.insertAtBegining(10);
+        linkedList.insertAtBegining(80);
+
+        linkedList.traverseSinglyList();
+    }
 }
 
-class LinkedNode{
-	int data;
-	LinkedNode next;
-	
-	public LinkedNode(int data) {
-		this.data=data;
-		this.next=next;
-		
-	}
+class LinkedNode {
+
+    int data;
+    LinkedNode next;
+
+    public LinkedNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
-class SinglyLinkedLIstDemo{
-	
-	LinkedNode head;
-	
-	public void insetAtBegining(int data) {
-		LinkedNode node=new LinkedNode(data);
-		if(head==null) {
-			head=node;
-			return;	
-		}
-		node.next=head;
-		head=node;
-		
-	}
-	
-	public void traverseSinglyList() {
-		if(head==null) {
-			System.out.println("list is empty");
-		}
-		
-		LinkedNode temp_head=head;
-		
-		while(temp_head!=null) {
-			System.out.println(temp_head.data);
-			temp_head=temp_head.next;
-			
-		}
-	}
+class SinglyLinkedListDemo {
 
+    LinkedNode head;
+
+    public void insertAtBegining(int data) {
+
+        LinkedNode node = new LinkedNode(data);
+
+        if (head == null) {
+            head = node;
+            return;
+        }
+
+        node.next = head;
+        head = node;
+    }
+
+    public void traverseSinglyList() {
+
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        LinkedNode temp_head = head;
+
+        while (temp_head != null) {
+
+            System.out.println(temp_head.data);
+
+            temp_head = temp_head.next;
+        }
+    }
 }
